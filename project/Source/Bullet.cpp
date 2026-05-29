e25ed619e666b55c728b56443e7da23bba7411bf
kokoro <a>
2026-05-29 19:46:09 +0900

    弾追加

--- project/Source/Bullet.cpp
+++ project/Source/Bullet.cpp
@@ -1,1 +1,25 @@
 #include "Bullet.h"
+#include "DxLib.h"
+
+void Bullet::Initialize()
+{
+	x = 0;
+	y = 0;
+	speed = 0;
+	isActive = false;
+}
+
+void Bullet::Update()
+{
+	if (!isActive)return;
+	y -= speed;
+
+	if (y < 0)
+	{
+		isActive = false;
+	}
+}
+void Bullet::Fire(int startX, int startY)
+{
+
+}
